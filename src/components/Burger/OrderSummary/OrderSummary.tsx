d2e52e95f49e50e0d7e383a3628c760e9186e8b1
Dung Ho <mclanren156@gmail.com>
2021-01-02 20:48:26 +0700

    155. Using Component Lifecycle Methods

--- src/components/Burger/OrderSummary/OrderSummary.tsx
+++ src/components/Burger/OrderSummary/OrderSummary.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 import { IIngredient } from '../BurgerIngredient/BurgerIngredient';
@@ -10,30 +10,38 @@ export interface IOrderSummaryProps {
   price: number
 };
 
-const orderSummary = (props: React.PropsWithChildren<IOrderSummaryProps>) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: 'capitalize' }}>
-            {igKey}
-          </span>: {props.ingredients[igKey]}
-        </li>);
-    });
+export interface IOrderSummaryState { };
 
-  return (
-    <Auxiliary>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Auxiliary>
-  );
+class OrderSummary extends Component<IOrderSummaryProps, IOrderSummaryState>  {
+  componentWillUpdate() {
+    console.log(`[OrderSummary] WillUpdate`);
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: 'capitalize' }}>
+              {igKey}
+            </span>: {this.props.ingredients[igKey]}
+          </li>);
+      });
+
+    return (
+      <Auxiliary>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Auxiliary>
+    );
+  }
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
@@ -9,22 +9,35 @@ export interface IModalProps {
   modalClosed: () => void
 };
 
-const modal = (props: React.PropsWithChildren<IModalProps>) => {
-  return (
-    <Auxiliary>
-      <Backdrop
-        show={props.show}
-        click={props.modalClosed} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-      </div>
-    </Auxiliary>
-  )
+export interface IModalState { };
+
+class Modal extends Component<IModalProps, IModalState> {
+  shouldComponentUpdate(nextProps: IModalProps, nextState: IModalState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate() {
+    console.log(`[Modal] WillUpdate`);
+
+  }
+
+  render() {
+    return (
+      <Auxiliary>
+        <Backdrop
+          show={this.props.show}
+          click={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Auxiliary>
+    )
+  }
 };
 
-export default modal;
+export default Modal;

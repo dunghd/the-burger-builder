4992fcf0247f92d1aee8074afd43ec224d93d733
Dung Ho <mclanren156@gmail.com>
2021-01-24 19:58:51 +0700

    1720. Refactoring Reducers Continued

--- src/store/reducers/burgerBuilder.tsx
+++ src/store/reducers/burgerBuilder.tsx
@@ -1,6 +1,6 @@
 import { IIngredient } from '../../components/Burger/BurgerIngredient/BurgerIngredient';
 import { Action, isType } from '../actions/actionTypes';
-import { addIngredientsAction, fetchIngredientsFailedAction, removeIngredientsAction, setIngredientsAction } from '../actions/burgerBuilder';
+import * as actions from '../actions/burgerBuilder';
 import { updateObject } from '../utility';
 
 export interface IBurgerReducerState {
@@ -31,46 +31,61 @@ const INGREDIENT_PRICES = {
   bacon: 0.7
 } as IIngredientPrice;
 
+const addIngredient = (state: any, action: Action<IBurgerReducerAction>) => {
+  const updatedIngredient = {
+    [action.payload.ingredientName]: state.ingredients[action.payload.ingredientName] + 1
+  };
+  const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+  const updateState = {
+    ingredients: updatedIngredients,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.payload.ingredientName]
+  } as IBurgerReducerState;
+  return updateObject(state, updateState);
+};
+
+const removeIngredient = (state: any, action: Action<IBurgerReducerAction>) => {
+  const updatedIngredient = {
+    [action.payload.ingredientName]: state.ingredients[action.payload.ingredientName] - 1
+  };
+  const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+  const updateState = {
+    ingredients: updatedIngredients,
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.payload.ingredientName]
+  } as IBurgerReducerState;
+  return updateObject(state, updateState);
+};
+
+const setIngredient = (state: any, action: Action<IBurgerReducerState> & Action<IIngredient>) => {
+  return updateObject(state, {
+    ingredients: {
+      salad: action.payload.salad,
+      bacon: action.payload.bacon,
+      cheese: action.payload.cheese,
+      meat: action.payload.meat,
+    } as IIngredient,
+    error: false,
+    totalPrice: 4
+  } as IBurgerReducerState);
+};
+
+const fetchIngredientsFailed = (state: any, action: Action<IBurgerReducerState>) => {
+  return updateObject(state, {
+    error: true
+  }) as IBurgerReducerState;
+};
 
 const reducer = (state: any = initialState, action: Action<IBurgerReducerState>): IBurgerReducerState => {
-  if (isType(action, addIngredientsAction)) {
-    const updatedIngredient = {
-      [action.payload.ingredientName]: state.ingredients[action.payload.ingredientName] + 1
-    };
-    const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
-    const updateState = {
-      ingredients: updatedIngredients,
-      totalPrice: state.totalPrice + INGREDIENT_PRICES[action.payload.ingredientName]
-    } as IBurgerReducerState;
-    return updateObject(state, updateState);
+  if (isType(action, actions.addIngredientsAction)) {
+    return addIngredient(state, action);
   }
-  else if (isType(action, removeIngredientsAction)) {
-    const updatedIngredient = {
-      [action.payload.ingredientName]: state.ingredients[action.payload.ingredientName] - 1
-    };
-    const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
-    const updateState = {
-      ingredients: updatedIngredients,
-      totalPrice: state.totalPrice + INGREDIENT_PRICES[action.payload.ingredientName]
-    } as IBurgerReducerState;
-    return updateObject(state, updateState);
+  else if (isType(action, actions.removeIngredientsAction)) {
+    return removeIngredient(state, action);
   }
-  else if (isType(action, setIngredientsAction)) {
-    return updateObject(state, {
-      ingredients: {
-        salad: action.payload.salad,
-        bacon: action.payload.bacon,
-        cheese: action.payload.cheese,
-        meat: action.payload.meat,
-      } as IIngredient,
-      error: false,
-      totalPrice: 4
-    } as IBurgerReducerState);
+  else if (isType(action, actions.setIngredientsAction)) {
+    return setIngredient(state, action);
   }
-  else if (isType(action, fetchIngredientsFailedAction)) {
-    return updateObject(state, {
-      error: true
-    }) as IBurgerReducerState;
+  else if (isType(action, actions.fetchIngredientsFailedAction)) {
+    return fetchIngredientsFailed(state, action);
   }
   else {
     return state;
--- src/store/reducers/order.tsx
+++ src/store/reducers/order.tsx
@@ -1,14 +1,6 @@
 import { IOrder } from '../../containers/BurgerBuilder/BurgerBuilder';
 import { Action, isType } from '../actions/actionTypes';
-import {
-  fetchOrdersFailAction
-  , fetchOrdersStartAction
-  , fetchOrdersSuccessAction
-  , purchaseBurgerFailAction
-  , purchaseBurgerStartAction
-  , purchaseBurgerSuccessAction
-  , purchaseInitAction
-} from '../actions/order';
+import * as actions from '../actions/order';
 import { updateObject } from '../utility';
 
 export interface IOrderReducerState {
@@ -25,44 +17,73 @@ const initialState = {
   error: null
 } as IOrderReducerState;
 
+const purchaseInit = (state: any, action: Action<actions.IPurchaseInitAction>) => {
+  return updateObject(state, {
+    purchased: action.payload.purchased
+  } as IOrderReducerState);
+};
+
+const purchaseBurgerStart = (state: any, action: Action<actions.IPurchaseBurgerStartAction>) => {
+  return updateObject(state, {
+    loading: action.payload.loading
+  } as IOrderReducerState);
+};
+
+const purchaseBurgerSuccess = (state: any, action: Action<actions.IPurchaseBurgerSuccessAction>) => {
+  const newOrder = updateObject(
+    action.payload.orderData, { id: action.payload.id });
+  return updateObject(state, {
+    loading: false,
+    purchased: true,
+    orders: state.orders.concat(newOrder)
+  } as IOrderReducerState);
+};
+
+const fetchOrdersStart = (state: any, action: Action<actions.IFetchOrdersStartAction>) => {
+  return updateObject(state, {
+    loading: action.payload.loading
+  } as IOrderReducerState);
+};
+
+const fetchOrdersSuccess = (state: any, action: Action<actions.IFetchOrdersSuccessAction>) => {
+  return updateObject(state, {
+    orders: action.payload.orders
+  } as IOrderReducerState);
+};
+
+const fetchOrdersFail = (state: any, action: Action<actions.IFetchOrdersFailAction>) => {
+  return updateObject(state, {
+    loading: action.payload.loading,
+    error: action.payload.error
+  } as IOrderReducerState);
+};
+
+const purchaseBurgerFail = (state: any, action: Action<actions.IPurchaseBurgerFailAction>) => {
+  return updateObject(state, {
+    loading: action.payload.loading
+  } as IOrderReducerState);
+};
+
 const reducer = (state: any = initialState, action: Action<any>): IOrderReducerState => {
-  if (isType(action, purchaseBurgerSuccessAction)) {
-    const newOrder = updateObject(action.payload.orderData, { id: action.payload.id });
-    return updateObject(state, {
-      loading: false,
-      purchased: true,
-      orders: state.orders.concat(newOrder)
-    } as IOrderReducerState);
+  if (isType(action, actions.purchaseBurgerSuccessAction)) {
+    return purchaseBurgerSuccess(state, action);
   }
-  else if (isType(action, purchaseInitAction)) {
-    return updateObject(state, {
-      purchased: action.payload.purchased
-    } as IOrderReducerState);
+  else if (isType(action, actions.purchaseInitAction)) {
+    return purchaseInit(state, action);
   }
-  else if (isType(action, fetchOrdersStartAction)) {
-    return updateObject(state, {
-      loading: action.payload.loading
-    } as IOrderReducerState);
+  else if (isType(action, actions.fetchOrdersStartAction)) {
+    return fetchOrdersStart(state, action);
   }
-  else if (isType(action, fetchOrdersSuccessAction)) {
-    return updateObject(state, {
-      orders: action.payload.orders
-    } as IOrderReducerState);
+  else if (isType(action, actions.fetchOrdersSuccessAction)) {
+    return fetchOrdersSuccess(state, action);
   }
-  else if (isType(action, fetchOrdersFailAction)) {
-    return updateObject(state, {
-      loading: action.payload.loading,
-      error: action.payload.error
-    } as IOrderReducerState);
+  else if (isType(action, actions.fetchOrdersFailAction)) {
+    return fetchOrdersFail(state, action);
   }
-  else if (isType(action, purchaseBurgerStartAction)) {
-    return updateObject(state, {
-      loading: action.payload.loading
-    } as IOrderReducerState);
-  } else if (isType(action, purchaseBurgerFailAction)) {
-    return updateObject(state, {
-      loading: action.payload.loading
-    } as IOrderReducerState);
+  else if (isType(action, actions.purchaseBurgerStartAction)) {
+    return purchaseBurgerStart(state, action);
+  } else if (isType(action, actions.purchaseBurgerFailAction)) {
+    return purchaseBurgerFail(state, action);
   }
   else {
     return state;
